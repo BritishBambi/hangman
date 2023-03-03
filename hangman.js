735770e228a2236bd3a5ac929132921ab095bd2b
farhan3311 <farhansidat3311@gmail.com>
2023-03-03 11:31:25 +0000

    farhan's hangma n

--- hangman.js
+++ hangman.js
@@ -11,31 +11,50 @@ console.log(chosenWord)
     var guessesLeft = chosenWord.length
     var progressWord = []
     for (var i = 0; i < chosenWord.length; i++) {
-
         
-        progressWord.push('-')
-        var letter = prompt("What is your next letter? ");
-        guessesLeft -= 1 
+        //for (var i = 0; i <= chosenWord.length; i++) {
+            progressWord.push("-")
+            //guessesLeft += 1
 
-        if (chosenWord.indexOf(letter) != -1) { // if the character is found
+        //};
+        console.log(progressWord)
+        
+
+        var letter = prompt("What is your next letter? ");
+
+        if ((chosenWord.toLowerCase()).indexOf(letter) != -1) { // if the character is found
             for (var i = 0; i < chosenWord.length; i++) { // loop on all characters
-                if (chosenWord[i].toLowerCase() == letter.toLowerCase()) // if this is an occurance
+
+                if (chosenWord[i].toLowerCase() === letter.toLowerCase()) { // if this is an occurance
+                    console.log(chosenWord[i])  
+                    console.log (progressWord[i])
+                    console.log (progressWord)
+                    console.log("found")
+
                     progressWord[i] = chosenWord[i];
+                    guessesLeft -= 1 
+
+                }
             }
-        } else {
+            
+        }
+        else {
             // wrong choice
             console.log("wrong choice")
+            guessesLeft -= 1 
+
         }
-
-
         if (progressWord.indexOf('-') == -1) { // if there are no dashes left
             //win!
+
             console.log("You win!");
         } 
         else if (guessesLeft <= 0) {
             //player has lost
-            console.log("player has lost")
+            console.log("player has lost",guessesLeft)
         }
+
+        
 
 
     progressWord.join(" "); // will form a string from the array, with a space as as delimeter
