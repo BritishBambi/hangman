f69e88a07773ed886958302f661b1b1318721543
farhan3311 <farhansidat3311@gmail.com>
2023-03-03 13:05:21 +0000

    hangman terminal version

--- hangman.js
+++ hangman.js
@@ -1,64 +1,47 @@
-//hangman games?
-
 const prompt = require("prompt-sync")({ sigint: true });
-
 const wordArray = ["Farhan", "Jojo", "Bart", "Tom"]
 
-//random element from array?
 var chosenWord = wordArray[Math.floor(Math.random()*wordArray.length)];
-console.log(chosenWord)
- function hangman(chosenWord) {
+function hangman(chosenWord) {
     var guessesLeft = chosenWord.length
     var progressWord = []
     for (var i = 0; i < chosenWord.length; i++) {
-        
-        //for (var i = 0; i <= chosenWord.length; i++) {
-            progressWord.push("-")
-            //guessesLeft += 1
+        progressWord.push("-")
+    }
+    for (var i = 0; i < chosenWord.length; i++) {
 
-        //};
-        console.log(progressWord)
-        
+        var letter = prompt("What is your next letter? ")
+        guessesLeft -= 1 
 
-        var letter = prompt("What is your next letter? ");
+        if ((chosenWord.toLowerCase()).indexOf(letter) != -1) { 
+             for (var j = 0; j < chosenWord.length; j++) { 
 
-        if ((chosenWord.toLowerCase()).indexOf(letter) != -1) { // if the character is found
-            for (var i = 0; i < chosenWord.length; i++) { // loop on all characters
-
-                if (chosenWord[i].toLowerCase() === letter.toLowerCase()) { // if this is an occurance
-                    console.log(chosenWord[i])  
-                    console.log (progressWord[i])
-                    console.log (progressWord)
-                    console.log("found")
-
-                    progressWord[i] = chosenWord[i];
-                    guessesLeft -= 1 
-
+                if (chosenWord[j].toLowerCase() == letter.toLowerCase()) { 
+                    progressWord[j] = chosenWord[j];
+                    console.log("Right choice")
                 }
             }
-            
         }
         else {
-            // wrong choice
             console.log("wrong choice")
-            guessesLeft -= 1 
 
         }
-        if (progressWord.indexOf('-') == -1) { // if there are no dashes left
-            //win!
-
+        if (progressWord.indexOf('-') == -1) { 
             console.log("You win!");
+            break
         } 
         else if (guessesLeft <= 0) {
-            //player has lost
             console.log("player has lost",guessesLeft)
+            break
         }
-
-        
-
-
-    progressWord.join(" "); // will form a string from the array, with a space as as delimeter
+    
+        progressWord.join(" "); 
     }
   
 }
+
+
 hangman(chosenWord)
+
+
+
